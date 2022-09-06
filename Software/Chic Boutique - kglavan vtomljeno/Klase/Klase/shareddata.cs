1ec829ba012e81343df2063187f392d04cba7e0b
VidTomljenovic <80068983+VidTomljenovic@users.noreply.github.com>
2022-09-06 21:10:21 +0200

    Add files via upload

---
+++ Software/Chic Boutique - kglavan vtomljeno/Klase/Klase/shareddata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+namespace PI___INVENTura___kglavan_vtomljeno
+{
+    public static class shareddata
+    {
+        public static BindingList<itemmodel> Items { get; set; } = new BindingList<itemmodel>();
+    }
+}
